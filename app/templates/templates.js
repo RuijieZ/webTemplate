2b059ea31e3823f1d695fd2b99a5b3d547584e74
ruijie <ruijie@kanjoya.com>
2016-05-18 09:41:33 -0700

    added the template-details page content, new controller

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -5,9 +5,18 @@ angular.module('templateStore.templates', ['ngRoute'])
 		when('/templates', {
 			templateUrl: 'templates/templates.html',
 			controller: 'templatesController'
+		}).
+		when('/templates/:templateId', {
+			templateUrl: 'templates/template-details.html',
+			controller: 'templateDetailsController'
 		});
 }])
 
 .controller('templatesController', ['$scope', function($scope) {
 	console.log('templateController is running');
-}]);
+}])
+
+.controller('templateDetailsController', ['$scope', function($scope) {
+	console.log('templateDetailsController is running');
+}])
+
